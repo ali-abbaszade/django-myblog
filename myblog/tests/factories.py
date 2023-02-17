78deb9cf7330b0100fd484b838243ea5f7c34aba
Ali Abbaszade <abbasszade.a@gmail.com>
2023-02-17 16:42:46 +0330

    Tested home page

--- myblog/tests/factories.py
+++ myblog/tests/factories.py
@@ -23,3 +23,11 @@ class PostFactory(factory.django.DjangoModelFactory):
     slug = "x"
     author = factory.SubFactory(UserFactory)
     status = "published"
+
+    @factory.post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            self.tags.add(*extracted)
